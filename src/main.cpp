41185b725c8c39d8fc085708d23aa133c0191f1e
ParmarAayush <parmarayush1816@gmail.com>
2024-12-31 13:39:56 +0530

    break code into function, code worked

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-// #define WIFI_SSID "OPPO A77 Ayush"
-// #define WIFI_PASSWORD "123456789"
 #define API_KEY "AIzaSyAWMqOfjaS_W2B-Gi9ef2N3Nt8xmTwZNpw"
 #define DATABASE_URL "https://iot-devices-1f8c0-default-rtdb.firebaseio.com/"
 
@@ -27,18 +25,33 @@ struct SensorData
   int humidity;
   bool success;
 };
-// void connectWifi()
-// {
-//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-//   Serial.print("Connecting to Wifi....");
-//   while (WiFi.status() != WL_CONNECTED)
-//   {
-//     Serial.print(".");
-//     delay(300);
-//   }
-//   Serial.print("Connected with IP: ");
-//   Serial.println(WiFi.localIP());
-// }
+
+//helper functions 
+String floatToString(float value)
+{
+  char buffer[32];              // Adjust buffer size as needed
+  dtostrf(value, 1, 2, buffer); // Convert float to string with 2 decimal places
+  return String(buffer);
+}
+
+void connectWifi()
+{
+  WiFiManager wm;
+  bool res;
+  res = wm.autoConnect("AutoConnectAP", "password");
+  if (!res)
+  {
+    Serial.println("Failed to connect");
+    // ESP.restart();
+  }
+  else
+  {
+    // if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    
+  }
+}
+
 void configDatabase()
 {
   config.api_key = API_KEY;
@@ -71,8 +84,6 @@ SensorData getDataFromSensor()
     data.temperatureC = dht.getTemperature();     //  celsius
     data.temperatureF = dht.getTemperature(true); // fahrenheit if true celsius of false
     data.humidity = dht.getHumidity();
-    Serial.print("Temprature:");
-    Serial.println(data.temperatureC);
   }
   else
   {
@@ -84,66 +95,46 @@ SensorData getDataFromSensor()
   return data;
 }
 
-String floatToString(float value)
-{
-  char buffer[32];              // Adjust buffer size as needed
-  dtostrf(value, 1, 2, buffer); // Convert float to string with 2 decimal places
-  return String(buffer);
-}
-
-void sendDataToFirebase(float temp)
+void sendDataToFirebase(float temp, float humidity)
 {
   String floatString = floatToString(temp);
-  Serial.printf("From Function %f", temp);
-  Serial.print(temp);
+  Serial.printf("From Function %0.2f ", temp);
+  Serial.printf("From Function %0.2f ", humidity);
+  Serial.printf("\n");
 
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
-  {
-    if (Firebase.RTDB.setInt(&fbdo, "new/path", temp))
-    {
-      Serial.println("PASSED");
-      Serial.printf("PATH: %s\n", fbdo.dataPath().c_str());
-      Serial.printf("TYPE: %s\n", fbdo.dataType().c_str());
-    }
-    else
-    {
-      Serial.println("FAILED");
-      Serial.printf("REASON: %s\n", fbdo.errorReason().c_str());
-    }
-  }
-  else
-  {
-    Serial.println("FAILED");
-    Serial.printf("REASON: %s\n", fbdo.errorReason().c_str());
-  }
+  // if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  // {
+  //   if (Firebase.RTDB.setInt(&fbdo, "new/path", temp))
+  //   {
+  //     Serial.println("PASSED");
+  //     Serial.printf("PATH: %s\n", fbdo.dataPath().c_str());
+  //     Serial.printf("TYPE: %s\n", fbdo.dataType().c_str());
+  //   }
+  //   else
+  //   {
+  //     Serial.println("FAILED");
+  //     Serial.printf("REASON: %s\n", fbdo.errorReason().c_str());
+  //   }
+  // }
+  // else
+  // {
+  //   Serial.println("FAILED");
+  //   Serial.printf("REASON: %s\n", fbdo.errorReason().c_str());
+  // }
 }
 void setup()
 {
   Serial.begin(115200);
-  WiFiManager wm;
-  bool res;
-  res = wm.autoConnect("AutoConnectAP", "password");
-  if (!res)
-  {
-    Serial.println("Failed to connect");
-    // ESP.restart();
-  }
-  else
-  {
-    // if you get here you have connected to the WiFi
-    Serial.println("connected...yeey :)");
-  }
-  dht.begin();
-  // connectWifi();
-
+  connectWifi();
+  delay(1000);
   configDatabase();
+  delay(4000);
+  dht.begin();
 }
 
 void loop()
 {
   SensorData data = getDataFromSensor();
-  Serial.print("Sensor Data: ");
-  Serial.print(data.temperatureC);
-  sendDataToFirebase(data.temperatureC);
+  sendDataToFirebase(data.temperatureC, data.humidity);
   delay(3000);
 }
